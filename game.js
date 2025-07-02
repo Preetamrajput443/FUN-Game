2bdbe1c3783dc4da983dc466e7f1797c432c4159
preetamrajput443 / fungame <preetamrajput443@gmail.com>
2025-07-02 09:11:13 +0530

    Create game.js

---
+++ game.js
@@ -0,0 +1,6 @@
+function startGame() {
+  document.getElementById("gameArea").innerHTML = `
+    <p class="text-xl text-green-600 mb-4">Game Started! 🚀</p>
+    <p class="text-gray-700">Game logic will go here in Phase 2...</p>
+  `;
+}
